d7253e4c2e8a79d1be277b1afa477dcfeb08f161
CorJex <61286377+CorJex@users.noreply.github.com>
2021-01-17 15:01:01 +0300

    sad
    angularfire

---
+++ CorJex/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBKY8caev45PGi9XCdB3BD_vM9AgO8-fjI",
+    authDomain: "angularfinal-corjex.firebaseapp.com",
+    databaseURL: "https://angularfinal-corjex-default-rtdb.firebaseio.com",
+    projectId: "angularfinal-corjex",
+    storageBucket: "angularfinal-corjex.appspot.com",
+    messagingSenderId: "355367351186",
+    appId: "1:355367351186:web:aa58ec89853be60eb4c74b",
+    measurementId: "G-HQRP6C3DGP"
+  }
+};
